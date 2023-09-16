47f7230f5f80b37c910e8e6ab2781298ed0df5d2
github username <github email address>
2023-09-16 10:47:39 -0700

    add legend for desktop

---
+++ src/data/appetizers.js
@@ -0,0 +1,64 @@
+// {
+//   name: "",
+//   description: "",
+//   price: "",
+//   legend: "",
+// },
+const appetizers = [
+  {
+    name: "VIET EGG ROLLS (2PCS)",
+    description: "Pork, shrimp, taro, & bean thread noodle in egg roll wrapper",
+    price: "5.5",
+    legend: "",
+  },
+  {
+    name: "SPICY WONTONS",
+    description: "Pork and shrimp wontons with scallions in spicy sauce",
+    price: "7.5",
+    legend: "spicy"
+  },
+  {
+    name: "WINGS - ASIAN STYLE (6PCS/12PCS)",
+    description: "Choice of house fish sauce, korean-style spicy sauce, spicy garlic soy, honey sriracha, sweet chili sauce, or hot sauce",
+    price: "9/16",
+    legend: ""
+  },
+  {
+    name: "WONTON SOUP",
+    description: "Pork and shrimp wonton in a cup of chicken soup with fried onions and topped with green onions & cilantro",
+    price: "7.5",
+    legend: "",
+  },
+  {
+    name: "CRISPY WONTON TACOS (3PCS)",
+    description: "Choice of chicken, brisket, tofu or shrimp (+$2)",
+    price: "7.5",
+    legend: ""
+  },
+  {
+    name: "CRISPY CHEESY SHRIMP ROLL (3PCS)",
+    description: "Seasoned shrimp with cream cheese & green onions",
+    price: "7.5",
+    legend: "",
+  },
+  {
+    name: "EDAMAME",
+    description: "Steamed with sea salt. Garlic or spicy garlic (+$2)",
+    price: "R:6 L:8",
+    legend: "vegetarian",
+  },
+  {
+    name: "POT STICKERS (5PCS)",
+    description: "Choice of chicken, pork, or veggie. Pan-fried or steamed",
+    price: "6.5",
+    legend: "",
+  },
+  {
+    name: "SPRING ROLLS (2PCS)",
+    description: "Choice of pork/shrimp, shrimp, kbbq, grilled pork, chicken, tofu, or veggie",
+    price: "6.5",
+    legend: "",
+  },
+]
+
+export default appetizers
