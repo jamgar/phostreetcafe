46c656d90ea3845399fc7158a2cb93a965110af2
github username <github email address>
2023-09-18 09:33:24 -0700

    add entrees and banh mi

---
+++ src/data/banh-mi.js
@@ -0,0 +1,53 @@
+// {
+//   name: "",
+//   description: "",
+//   price_1: "",
+//   price_2: "",
+//   legend: "",
+// },
+const banhMi = [
+  {
+    name: "GRILLED PORK",
+    description: "Lemongrass grilled pork with pate with light spread of mayo, sliced cucumber, pickled carrots, jalapeño, & cilantro",
+    price_1: "8",
+    price_2: "",
+    legend: "",
+  },
+  {
+    name: "KOREAN-STYLE BBQ",
+    description: "Korean-style marinated beef, kimchi, & spicy mayo with sliced cucumber, pickled carrots, jalapeño, & cilantro",
+    price_1: "8",
+    price_2: "",
+    legend: "",
+  },
+  {
+    name: "BEEF BRISKET",
+    description: "Marinated shredded beef brisket with pate, light spread of mayo, sliced cucumber, pickled carrots, jalapeño, & cilantro",
+    price_1: "8",
+    price_2: "",
+    legend: "",
+  },
+  {
+    name: "GRILLED CHICKEN",
+    description: "Grilled chicken with pate, light spread of mayo, sliced cucumber, pickled carrots, jalapeño, & cilantro",
+    price_1: "8",
+    price_2: "",
+    legend: "",
+  },
+  {
+    name: "VEGAN",
+    description: "Fried lemongrass tofu with house soy sauce, sliced cucumber, pickled carrots, jalapeño, & cilantro",
+    price_1: "8",
+    price_2: "",
+    legend: "vegetarian",
+  },
+  {
+    name: "PORK ROLL",
+    description: "Vietnamese pork roll, pate, & house soy sauce with light spread of mayo, sliced cucumber, pickled carrots, jalapeño, & cilantro",
+    price_1: "8",
+    price_2: "",
+    legend: "",
+  },
+]
+
+export default banhMi 
